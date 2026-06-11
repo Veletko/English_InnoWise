c19edceacfeb3a41431aa4b0f4cc12d1171e4159
Vadim Volkov <vadimvvipp@gmail.com>
2026-06-11 11:16:33 +0300

    feat: add Entities and Enums

---
+++ services/IdentityService/IdentityService.Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using IdentityService.Domain.Enums;
+
+namespace IdentityService.Domain.Entities;
+
+public class User : IdentityUser<Guid>
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+}
---
+++ services/IdentityService/IdentityService.Domain/Enums/UserRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityService.Domain.Enums
+{
+    public enum UserRole
+    {
+        None = 0,
+        Student = 1,
+        Teacher = 2,
+        Admin = 3
+    }
+}

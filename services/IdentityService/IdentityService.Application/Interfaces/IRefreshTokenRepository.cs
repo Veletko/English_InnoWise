7e53f15badec1e7b72e54d9454abffa16fceeae3
Vadim Volkov <vadimvvipp@gmail.com>
2026-06-17 21:08:08 +0300

    feat: add RefreshTokenRepository

--- services/IdentityService/IdentityService.Application/Interfaces/IRefreshTokenRepository.cs
+++ services/IdentityService/IdentityService.Application/Interfaces/IRefreshTokenRepository.cs
@@ -7,5 +7,5 @@ public interface IRefreshTokenRepository
     Task<RefreshToken?> GetByTokenAsync(string token, CancellationToken cancellationToken);
     Task AddAsync(RefreshToken refreshToken, CancellationToken cancellationToken);
     Task UpdateAsync(RefreshToken refreshToken, CancellationToken cancellationToken);
-    Task DeleteAsync(RefreshToken refreshToken, CancellationToken cancellationToken);
 }
+
---
+++ services/IdentityService/IdentityService.Infrastructure/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,27 @@
+﻿using IdentityService.Application.Interfaces;
+using IdentityService.Domain.Entities;
+using IdentityService.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityService.Infrastructure.Repositories;
+
+public class RefreshTokenRepository(ApplicationDbContext context) : IRefreshTokenRepository
+{
+    private readonly ApplicationDbContext _context = context;
+    public async Task<RefreshToken?> GetByTokenAsync(string token, CancellationToken cancellationToken)
+    {
+        return await  _context.RefreshTokens.FirstOrDefaultAsync(r => r.Token == token, cancellationToken);
+    }
+
+    public async Task AddAsync(RefreshToken refreshToken, CancellationToken cancellationToken)
+    {
+        await _context.RefreshTokens.AddAsync(refreshToken, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task UpdateAsync(RefreshToken refreshToken, CancellationToken cancellationToken)
+    {
+        _context.RefreshTokens.Update(refreshToken);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}

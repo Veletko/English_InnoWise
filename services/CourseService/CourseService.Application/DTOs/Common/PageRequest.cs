d3cf7c6caa4576da1299cd0ae63e67124dbaf85f
Vadim Volkov <vadimvvipp@gmail.com>
2026-08-25 18:37:42 +0300

    feat: add page contracts

---
+++ services/CourseService/CourseService.Application/DTOs/Common/PageRequest.cs
@@ -0,0 +1,23 @@
+﻿using CourseService.Application.Constants;
+
+namespace CourseService.Application.DTOs.Common;
+
+public record PageRequest
+{
+    private const int MaxPageSize = 50;
+    private readonly int _pageSize = PageConsts.DefaultPageSize;
+    private readonly int _pageNumber = PageConsts.DefaultPageNumber;
+
+    public int PageNumber
+    {
+        get => _pageNumber;
+        init => _pageNumber = Math.Max(PageConsts.DefaultPageNumber, value);
+    }
+
+    public int PageSize
+    {
+        get => _pageSize;
+        init => _pageSize = value > MaxPageSize ? MaxPageSize
+            : Math.Max(PageConsts.DefaultPageSize, value);
+    }
+}
---
+++ services/CourseService/CourseService.Application/DTOs/Common/PagedResult.cs
@@ -0,0 +1,23 @@
+﻿using CourseService.Domain.Entities;
+
+namespace CourseService.Application.DTOs.Common;
+
+public record PagedResult<TEntity>
+{
+    public IReadOnlyCollection<TEntity> Items { get; init; }
+    public int PageNumber { get; init; }
+    public int PageSize { get; init; }
+    public int TotalCount { get; init; }
+    
+    public int TotalPages => PageSize > 0 ? (int)Math.Ceiling(TotalCount / (double)PageSize) : 0;
+    public bool HasPreviousPage => PageNumber > 1;
+    public bool HasNextPage => PageNumber < TotalPages;
+    
+    public PagedResult(IReadOnlyCollection<TEntity> items, int totalCount, int pageNumber, int pageSize)
+    {
+        Items = items;
+        TotalCount = totalCount;
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+    }
+}
